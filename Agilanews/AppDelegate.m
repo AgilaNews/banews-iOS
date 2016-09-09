fc4336b1d0ce0d1a568de5b5104bb8d1b0464e70
张思思 <280361299@qq.com>
2016-09-09 19:10:20 +0800

    修改弹窗bug

--- Agilanews/AppDelegate.m
+++ Agilanews/AppDelegate.m
@@ -116,7 +116,10 @@
             }
         }
     }
-
+    // 刷新页面
+    for (HomeTableViewController *homeTabVC in homeVC.segmentVC.subViewControllers) {
+        [homeTabVC.tableView reloadData];
+    }
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
