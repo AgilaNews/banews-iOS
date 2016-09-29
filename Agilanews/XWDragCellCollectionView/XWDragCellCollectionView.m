0cd6a995a96334e639160c02f2a9186809d36747
张思思 <280361299@qq.com>
2016-09-29 11:29:08 +0800

    动画修改

--- Agilanews/XWDragCellCollectionView/XWDragCellCollectionView.m
+++ Agilanews/XWDragCellCollectionView/XWDragCellCollectionView.m
@@ -103,6 +103,9 @@ typedef NS_ENUM(NSUInteger, XWDragCellCollectionViewScrollDirection) {
     cell.hidden = YES;
     _tempMoveCell = tempMoveCell;
     _tempMoveCell.frame = cell.frame;
+    [UIView animateWithDuration:.25 animations:^{
+        _tempMoveCell.frame = CGRectMake(cell.left, cell.top, cell.width * 1.3, cell.height * 1.3);
+    }];
     [self addSubview:_tempMoveCell];
     //开启边缘滚动定时器
     [self xwp_setEdgeTimer];
@@ -162,6 +165,7 @@ typedef NS_ENUM(NSUInteger, XWDragCellCollectionViewScrollDirection) {
     }
     [UIView animateWithDuration:0.25 animations:^{
         _tempMoveCell.center = cell.center;
+        _tempMoveCell.frame = cell.frame;
     } completion:^(BOOL finished) {
         [self xwp_stopShakeAllCell];
         [_tempMoveCell removeFromSuperview];
