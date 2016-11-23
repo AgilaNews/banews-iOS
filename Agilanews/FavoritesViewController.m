5cb06f0f0ab38c6cdcb068e8e57f1362461e6098
张思思 <280361299@qq.com>
2016-11-23 10:52:17 +0800

    fix bug

--- Agilanews/FavoritesViewController.m
+++ Agilanews/FavoritesViewController.m
@@ -655,6 +655,9 @@
  */
 - (void)tableView:(UITableView *)tableView didEndDisplayingCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    if (indexPath.row >= _dataList.count) {
+        return;
+    }
     NewsModel *model = _dataList[indexPath.row];
     if (model.tpl.integerValue == NEWS_OnlyVideo && [cell isKindOfClass:[OnlyVideoCell class]]) {
         ((OnlyVideoCell *)cell).isPlay = NO;
