dda079437e396d86ad672aafe658102ea3b5fcfa
张思思 <280361299@qq.com>
2016-12-27 19:44:18 +0800

    修改评论数小红点

--- Agilanews/VideoDetailViewController.m
+++ Agilanews/VideoDetailViewController.m
@@ -546,7 +546,7 @@
                 _commentsLabel.text = @"999+";
             }
             CGSize commentSize = [_detailModel.commentCount.stringValue calculateSize:CGSizeMake(40, 10) font:_commentsLabel.font];
-            _commentsLabel.width = commentSize.width + 5;
+            _commentsLabel.width = MAX(commentSize.width + 5, 10);
         }
         [_tableView reloadData];
         [_commentTextView.textView resignFirstResponder];
@@ -1527,7 +1527,7 @@
                         _commentsLabel.text = @"999+";
                     }
                     CGSize commentSize = [_detailModel.commentCount.stringValue calculateSize:CGSizeMake(40, 10) font:_commentsLabel.font];
-                    _commentsLabel.width = commentSize.width + 5;
+                    _commentsLabel.width = MAX(commentSize.width + 5, 10);
                 } else {
                     _commentsLabel.hidden = YES;
                 }
@@ -1802,7 +1802,7 @@
                         _commentsLabel.text = @"999+";
                     }
                     CGSize commentSize = [_detailModel.commentCount.stringValue calculateSize:CGSizeMake(40, 10) font:_commentsLabel.font];
-                    _commentsLabel.width = commentSize.width + 5;
+                    _commentsLabel.width = MAX(commentSize.width + 5, 10);
                 }
             }
         }
